dea5d0b269110589db8da8e6a1f72c30e143e0c1
pikaChin <derrfgh4563@gmail.com>
2019-10-17 01:29:59 +0800

    second activity can go back

--- app/src/main/java/com/example/intent/Second.java
+++ app/src/main/java/com/example/intent/Second.java
@@ -2,13 +2,24 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Second extends AppCompatActivity {
+public class Second extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button goBackBtn = findViewById(R.id.backToFirst);
+        goBackBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
     }
 }
