20aee3ccbe75e1e7ceda4a59c33acc8378ba186f
pikaChin <derrfgh4563@gmail.com>
2019-10-17 01:25:16 +0800

    can switch to second activity

--- app/src/main/java/com/example/intent/MainActivity.java
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button switchBtn = findViewById(R.id.switchBtn);
+        switchBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, Second.class));
     }
 }
